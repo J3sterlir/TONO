f869696292ed719cf3fd5e089ac8dc6417d4cbd2
Jest <jeremiah.lirag@gmail.com>
2026-08-13 12:20:38 +0800

    nuxt config revert

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,16 +5,8 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ['motion-v/nuxt', "@nuxt/icon", "@nuxtjs/supabase"],
-  runtimeConfig: {
-    public: {
-      supabaseUrl: process.env.NUXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL,
-      supabaseKey: process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_KEY,
-    },
-  },
   supabase: {
     redirect: false,
-    url: process.env.NUXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL,
-    key: process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_KEY,
   },
   icon: {
     serverBundle: {
